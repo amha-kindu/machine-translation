53cec0a9d72b917678a4057806b16f324c08dd38
Amha <amhaznif@gmail.com>
2024-02-03 17:43:53 +0300

    added pyqt5 interface for inference

--- inference.py
+++ inference.py
@@ -1,10 +1,14 @@
+import sys
 import torch
 import random
 from config import *
 from enum import Enum
+from PyQt5.QtGui import QFont
 from tokenizers import Tokenizer
-from dataset import ParallelTextDataset
 from model import MtTransformerModel
+from dataset import ParallelTextDataset
+from train import get_model, get_tokenizer
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QGridLayout
 
 
 class SamplingStrategy(Enum):
@@ -25,7 +29,7 @@ class MtInferenceEngine:
         self.pad_token = torch.tensor([self.src_tokenizer.token_to_id("[PAD]")], dtype=torch.int64, device=DEVICE)  # (1,)
         self.model.eval()
         
-    def translate(self, source_text: str, seq_len: int, strategy=SamplingStrategy.GREEDY) -> tuple[str, str]:
+    def translate(self, source_text: str, max_len: int, strategy=SamplingStrategy.GREEDY) -> tuple[str, str]:
         dataset = ParallelTextDataset(
             dataset=[{"en": source_text, "am":"" }], 
             src_tokenizer=self.src_tokenizer,
@@ -38,7 +42,7 @@ class MtInferenceEngine:
         encoder_mask = batch["encoder_mask"].to(DEVICE)         # (1, 1, 1, seq_len) 
         decoder_mask = batch["decoder_mask"].to(DEVICE)         # (1, 1, seq_len, seq_len) 
                         
-        return self.translate_raw(encoder_input, encoder_mask, decoder_mask, seq_len, strategy)
+        return self.translate_raw(encoder_input, encoder_mask, decoder_mask, max_len, strategy)
 
     @torch.no_grad()
     def translate_raw(self, encoder_input: torch.Tensor, encoder_mask: torch.Tensor, decoder_mask: torch.Tensor, max_len: int, strategy=SamplingStrategy.GREEDY) -> str:        
@@ -48,7 +52,7 @@ class MtInferenceEngine:
         # Initialize the decoder input with the start of sentence token
         decoder_input = torch.concat([
             self.sos_token,                                                                      # (1,)
-            torch.tensor([self.pad_token] * (max_len - 1), dtype=torch.int64, device=DEVICE)     # (max_seq_len - 1,)
+            torch.tensor([self.pad_token] * (SEQ_LEN - 1), dtype=torch.int64, device=DEVICE)     # (max_seq_len - 1,)
         ]).unsqueeze(0)   
         
         count = 1
@@ -88,3 +92,66 @@ class MtInferenceEngine:
         decoder_input = decoder_input.squeeze(0)                                    # torch.tensor([...]) with shape tensor.Size([max_len])
     
         return self.tgt_tokenizer.decode(decoder_input.detach().cpu().tolist())
+    
+
+class TranslationApp(QWidget):
+    def __init__(self, inference_engine: MtInferenceEngine):
+        super().__init__()
+        self.init_ui()
+        self.inference_engine = inference_engine
+
+    def init_ui(self):
+        self.setWindowTitle('Translation App')
+        self.setGeometry(100, 100, 600, 400)
+
+        self.input_label = QLabel('Input(English):')
+        self.input_label.setFont(QFont('Arial', 12))  
+        self.input_textbox = QLineEdit(self)
+        self.input_textbox.setFont(QFont('Nyala', 14))  
+        self.input_textbox
+        self.input_textbox.returnPressed.connect(self.on_translate_button_clicked)
+
+        self.output_label = QLabel('Output(Amharic):')
+        self.output_label.setFont(QFont('Arial', 12))  
+        self.output_textbox = QLineEdit(self)
+        self.output_textbox.setReadOnly(True)
+        self.output_textbox.setFont(QFont('Nyala', 14)) 
+
+        self.translate_button = QPushButton('Translate', self)
+        self.translate_button.setFont(QFont('Arial', 12))
+        self.translate_button.setFixedWidth(self.width() // 2)
+        self.translate_button.clicked.connect(self.on_translate_button_clicked)
+
+        layout = QGridLayout()
+        layout.addWidget(self.input_label, 0, 0)
+        layout.addWidget(self.input_textbox, 0, 1)
+        layout.addWidget(self.output_label, 1, 0)
+        layout.addWidget(self.output_textbox, 1, 1)
+        layout.addWidget(self.translate_button, 2, 1, 1, 2)
+
+        self.setLayout(layout)
+
+    def on_translate_button_clicked(self):
+        input_text = self.input_textbox.text()
+        prediction: str = self.inference_engine.translate(input_text, 10)
+        self.output_textbox.setText(prediction)
+
+if __name__ == '__main__':
+    vocab_size = 6000
+    app = QApplication(sys.argv)
+    state = torch.load("./models/tmodel_v1.pt", map_location=DEVICE)
+    
+    src_tokenizer: Tokenizer = get_tokenizer(SRC_LANG, "tokenizer-en-v3.5-6k.json")
+    tgt_tokenizer: Tokenizer = get_tokenizer(TGT_LANG, "tokenizer-am-v3.5-6k.json")
+
+    print(src_tokenizer.decode(src_tokenizer.encode("What is your name?").ids))
+    
+    model = get_model(vocab_size, vocab_size).to(DEVICE)
+    model.load_state_dict(state["model_state_dict"])
+    
+    model.eval()
+    inference_engine = MtInferenceEngine(model, src_tokenizer, tgt_tokenizer)
+    
+    translation_app = TranslationApp(inference_engine)
+    translation_app.show()
+    sys.exit(app.exec_())
